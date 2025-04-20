4024120dfce8c74e126db37b336985d021c830a6
omnia99-hub <omniadafalla07@gmail.com>
2025-04-19 23:38:49 -0600

    Add R package files to master branch

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 # Suppress global variable notes during R CMD check
 utils::globalVariables(c(
-  "stocksr", "Date", "returns", "sector_index", "R2", "IncNodePurity",
+  "stocksr", "Date", "returns", "sector_index", "R²", "IncNodePurity",
   ".", "%>%","sd", "desc", "complete.cases", "Sector", "RMSE", "theme",
   "Value", "Type", "Feature"
 ))
